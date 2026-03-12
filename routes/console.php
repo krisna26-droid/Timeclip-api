01b7404dfadc726935e580741bf5b66d2764c224
Krisnaadiguna <krisnaaddd026@gmail.com>
2026-03-12 14:01:06 +0800

    feat: Add scheduled command for queue worker with overlapping prevention

--- routes/console.php
+++ routes/console.php
@@ -9,3 +9,7 @@ Schedule::command('credits:reset')->dailyAt('00:00');
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::command('queue:work --stop-when-empty --timeout=300')
+    ->everyMinute()
+    ->withoutOverlapping();
