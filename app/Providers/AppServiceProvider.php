6f8e6926234e4842cc787be45d923ce98b2482b3
Krisnaadiguna <krisnaaddd026@gmail.com>
2026-03-12 10:53:16 +0800

    update providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Broadcast; // Tambahkan ini
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Ini akan mengubah rute menjadi /api/broadcasting/auth
+        // Dan memaksanya pakai pengamanan Sanctum
+        Broadcast::routes([
+            'prefix' => 'api',
+            'middleware' => ['api', 'auth:sanctum']
+        ]);
+
+        require base_path('routes/channels.php');
     }
 }
