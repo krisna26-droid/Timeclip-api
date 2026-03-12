adff377ab5837552d154447c00e17ec059cbc320
Krisnaadiguna <krisnaaddd026@gmail.com>
2026-03-12 11:02:03 +0800

    edit channels

--- routes/channels.php
+++ routes/channels.php
@@ -2,6 +2,12 @@
 
 use Illuminate\Support\Facades\Broadcast;
 
-Broadcast::channel('user.{userId}', function ($user, $userId) {
-    return (int) $user->id === (int) $userId;
+// Versi 1: Format Lengkap (Sesuai permintaan FE & standar Laravel)
+Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
+});
+
+// Versi 2: Format Pendek (Hanya untuk jaga-jaga)
+Broadcast::channel('user.{id}', function ($user, $id) {
+    return (int) $user->id === (int) $id;
 });
