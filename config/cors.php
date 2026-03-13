bb24e0a8fcd762d6d323de56c2ff8c75254a2df8
Krisnaadiguna <krisnaaddd026@gmail.com>
2026-03-13 10:05:31 +0800

    feat: Add ngrok URL to allowed origins in CORS configuration

--- config/cors.php
+++ config/cors.php
@@ -16,6 +16,7 @@ return [
         'http://127.0.0.1:5173',
         'http://localhost:3000',
         'http://127.0.0.1:3000',
+        'https://bradly-spumescent-keisha.ngrok-free.dev'
     ],
 
     'allowed_origins_patterns' => [],
